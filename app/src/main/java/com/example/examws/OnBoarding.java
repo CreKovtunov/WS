86984616ed7c58feb91c4a9465126fa741e2b2aa
kovtunovivan <kovtunovtn@gmail.com>
2022-12-12 18:05:46 +0300

    Start project with all empty activity

---
+++ app/src/main/java/com/example/examws/OnBoarding.java
@@ -0,0 +1,28 @@
+package com.example.examws;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class OnBoarding extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+        findViewById(R.id.button_on_boarding).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+        findViewById(R.id.textView_on_boarding).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
+    }
+}
